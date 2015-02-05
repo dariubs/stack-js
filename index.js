48028c4a14d128b4292ea6ac7c9737482e6ab131
Dariush Abbasi <dariushem@yahoo.com>
2015-02-06 00:09:22 +0330

    complete functions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Stackjs = function(){
     //this._type = type; //type of implementation
     this._storage = []; //array
     this._pointer = -1;
+    this._temp;
 }
 
 Stackjs.prototype.push = function(data){
@@ -16,5 +17,30 @@ Stackjs.prototype.pop = function(){
         this._pointer--;
     }else {
         this._pointer = -1;
+    }
+}
+
+Stackjs.prototype.peek = function(){
+    if(this._pointer >= 0){
+        this._temp = this._storage[this._pointer];
+        this._storage.pop();
+        this._pointer--;
+        
+        return this._temp;
+    }else {
+        this._pointer = -1;
+    }
+}
+
+Stackjs.prototype.clear = function(){
+    this._storage = [];
+    this._pointer = -1;
+}
+
+Stackjs.prototype.length = function() {
+    if(this._pointer >= 0){
+        return (this._pointer + 1);
+    } else {
+        return 0;
     }
 }
