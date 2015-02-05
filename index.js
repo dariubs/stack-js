04605aee532b2faca18b33295acd289064baeedf
Dariush Abbasi <dariushem@yahoo.com>
2015-02-06 00:33:40 +0330

    add and apply jshint

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var Stackjs = function(){
     //this._type = type; //type of implementation
     this._storage = []; //array
     this._pointer = -1;
-    this._temp;
-}
+    this._temp = 0;
+};
 
 Stackjs.prototype.push = function(data){
     this._storage.push(data);
     this._pointer++;
-}
+};
 
 Stackjs.prototype.pop = function(){
     if(this._pointer >= 0){
@@ -18,7 +18,7 @@ Stackjs.prototype.pop = function(){
     }else {
         this._pointer = -1;
     }
-}
+};
 
 Stackjs.prototype.peek = function(){
     if(this._pointer >= 0){
@@ -30,12 +30,12 @@ Stackjs.prototype.peek = function(){
     }else {
         this._pointer = -1;
     }
-}
+};
 
 Stackjs.prototype.clear = function(){
     this._storage = [];
     this._pointer = -1;
-}
+};
 
 Stackjs.prototype.length = function() {
     if(this._pointer >= 0){
@@ -43,4 +43,4 @@ Stackjs.prototype.length = function() {
     } else {
         return 0;
     }
-}
+};
