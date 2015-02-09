3fbbe9ae9fee27f2557d3573b5a9553ab2ed66a9
Dariush Abbasi <dariushem@yahoo.com>
2015-02-09 23:17:02 +0330

    Document & add show function

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-
+/**
+*Initialize a new stack
+*
+*@api public
+*/
 var Stackjs = function(){
-    //this._type = type; //type of implementation
-    this._storage = []; //array
-    this._pointer = -1;
-    this._temp = 0;
+    this._storage = []; //Strorage of stack items
+    this._pointer = -1; //pointer
+    this._temp = 0; //use for temporary values
 };
 
+/**
+*Add a item to stack
+*
+*@param {String} data
+*@api public
+*/
 Stackjs.prototype.push = function(data){
     this._storage.push(data);
     this._pointer++;
 };
 
+
+/**
+*Remove item from stack
+*
+*@api public
+*/
 Stackjs.prototype.pop = function(){
     if(this._pointer >= 0){
         this._storage.pop();
@@ -20,6 +35,12 @@ Stackjs.prototype.pop = function(){
     }
 };
 
+
+/**
+*Peek item from stack
+*
+*@api public
+*/
 Stackjs.prototype.peek = function(){
     if(this._pointer >= 0){
         this._temp = this._storage[this._pointer];
@@ -32,11 +53,33 @@ Stackjs.prototype.peek = function(){
     }
 };
 
+/**
+*Return item from stack
+*
+*@api public
+*/
+Stackjs.prototype.show = function(){
+    if(this._pointer >= 0){
+        return this._storage[this._storage.length];
+    }
+};
+
+/**
+*Remove all stack items
+*
+*@api public
+*/
 Stackjs.prototype.clear = function(){
     this._storage = [];
     this._pointer = -1;
 };
 
+
+/**
+*get length of stack
+*
+*@api public
+*/
 Stackjs.prototype.length = function() {
     if(this._pointer >= 0){
         return (this._pointer + 1);
